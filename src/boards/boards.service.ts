ddcf5aa69308457cb9389e1d6d222d97447d8e04
R3D1NM <r3dinm@gmail.com>
2023-12-23 15:40:36 +0900

    [feat] getAllBoard() & createBoard()

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import {v1 as uuid} from 'uuid';
+
+@Injectable()
+export class BoardsService {
+    private boards: Board[] = [];
+
+    //Get all board list
+    getAllBoards() : Board[] {
+        return this.boards;
+    }
+
+    createBoard(title:string, description:string){
+        const board : Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+        this.boards.push(board);
+        return board;
+    }
+}
