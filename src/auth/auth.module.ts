d68a343d366b04b15b711a65973959c477f6b579
R3D1NM <r3dinm@gmail.com>
2023-12-26 22:18:19 +0900

    [feat] issue jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,19 @@ import { AuthService } from './auth.service';
 import { User } from 'src/entity/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import 'dotenv/config'
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService,UserRepository],
-  imports: [TypeOrmModule.forFeature([User])]
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.register({
+    secret: process.env.JWT_SECRET,
+    signOptions:{
+      expiresIn: 60*60
+    }
+  })
+  ]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,27 +3,30 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     // Inject Repository
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository : UserRepository
+        private userRepository : UserRepository,
+        private jwtService : JwtService
     ){}
 
     async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
         return this.userRepository.createUser(authCredentialDto);
     }
     
-    async signIn(authCredentialDto: AuthCredentialDto) : Promise<string> {
-        const {username, password} = authCredentialDto
-        const user = await this.userRepository.findOneBy({username})
-
-        if (user && (await bcrypt.compare(password,user.password))) {
-            return 'Login Success'
-        } else {
-            throw new UnauthorizedException("Login Failed")
-        }
+async signIn(authCredentialDto: AuthCredentialDto) : Promise<{accessToken : string}> {
+    const {username, password} = authCredentialDto
+    const user = await this.userRepository.findOneBy({username})
+    if (user && (await bcrypt.compare(password,user.password))) {
+        const payload = {username}
+        const accessToken = await this.jwtService.sign(payload)
+        return {accessToken}
+    } else {
+        throw new UnauthorizedException("Login Failed")
     }
+}
 }
